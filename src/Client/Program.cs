60624346c17705ad49633a209c11772a1cd0d2b5
LuckyZhangWeiwei <zhangzhang_10@hotmail>
2024-09-27 15:29:12 +0800

    Add project files.

---
+++ src/Client/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+//https://docs.duendesoftware.com/identityserver/v7/quickstarts/1_client_credentials/
+
+using IdentityModel.Client;
+using System.Text.Json;
+
+var client = new HttpClient();
+
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+if (!disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    Console.WriteLine(disco.Exception);
+}
+
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+{
+    Address = disco.TokenEndpoint,
+    ClientId = "client",
+    ClientSecret = "secret",
+    Scope = "api1"
+});
+
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    Console.WriteLine(tokenResponse.ErrorDescription);
+}
+
+Console.WriteLine(tokenResponse.AccessToken);
+
+// call api
+//var apiClient = new HttpClient();
+//var disco = await apiClient.GetDiscoveryDocumentAsync("https://localhost:5001");
+//var tokenResponse = await apiClient.RequestClientCredentialsTokenAsync(
+//    new ClientCredentialsTokenRequest
+//    {
+//        Address = disco.TokenEndpoint,
+//        ClientId = "client",
+//        ClientSecret = "secret",
+//        Scope = "api1",
+//    }
+//);
+client.SetBearerToken(tokenResponse.AccessToken!); // AccessToken is always non-null when IsError is false
+
+var response = await client.GetAsync("https://localhost:6001/identity");
+if (!response.IsSuccessStatusCode)
+{
+    Console.WriteLine(response.StatusCode);
+}
+
+var doc = JsonDocument.Parse(await response.Content.ReadAsStringAsync()).RootElement;
+Console.WriteLine(
+    JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true })
+);
+
+Console.ReadLine();
---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,43 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        [new IdentityResources.OpenId(), new IdentityResources.Profile()];
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        [new ApiScope(name: "api1", displayName: "My API")];
+
+    public static IEnumerable<Client> Clients =>
+        [
+            new Client
+            {
+                ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                AllowedScopes = { "api1" },
+            },
+            new Client
+            {
+                ClientId = "web",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+
+                // where to redirect to after login
+                RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                },
+            },
+        ];
+}
